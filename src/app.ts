74d78ef75c8e96256d69e652c55af5cac1026406
Thar Luke <gyit7159@gmail.com>
2025-04-29 12:47:45 +0630

    setup rateLimit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import express from 'express'
-import helmet from 'helmet'
-import compression from 'compression'
-import cors from 'cors'   //cross origin resource sharing(CORS)
-import morgan from 'morgan'
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors"; //cross origin resource sharing(CORS)
+import morgan from "morgan";
+import { limiter } from "./middlewares/rateLimiter";
 
-export const app = express()
+export const app = express();
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cors())
+app
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(morgan("dev"))
+  .use(cors())
+  .use(helmet())
+  .use(compression())
+  .use(limiter)
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: "Server is alright"})
+})
