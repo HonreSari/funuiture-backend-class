2d2a5ca8d89de4104879d24113701ff578892ed9
Thar Luke <gyit7159@gmail.com>
2025-05-12 09:22:23 +0630

    Changes some module

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import helmet from "helmet";
-import compression from "compression";
-import cors from "cors"; //cross origin resource sharing(CORS)
-import morgan from "morgan";
+
 import cookieParser from "cookie-parser";
 import { auth } from '../src/middlewares/auth'
 import authRoutes from "../src/routes/v1/auth";
@@ -13,19 +10,13 @@ import viewRoutes from "./routes/web/view";
 export const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 //auth routes
 app.use("/api/v1", authRoutes);
 app.use('/api/v1/admin', auth ,userRoutes)
 // ejs view templete
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
-app
-.use(morgan("dev"))
-.use(express.urlencoded({ extended: true }))
-.use(cors())
-.use(helmet())
-.use(compression());
-
 
 app.use(express.static("public"));
 app.use(viewRoutes);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config"
 import {app} from './app'
-
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors"; //cross origin resource sharing(CORS)
+import morgan from "morgan";
 
 const PORT = process.env.PORT 
+// These are "environment-aware" or global middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 app.listen(PORT, ( ) => {
     console.log(`Server is running on port ${PORT}`)
