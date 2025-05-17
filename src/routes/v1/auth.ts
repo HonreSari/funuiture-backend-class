cbf76b152784af2b3be2d37deebcbcb399d568dc
Thar Luke <gyit7159@gmail.com>
2025-05-17 21:49:20 +0630

    set route

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { confirmPassword, login, logout, register, verifyOtp } from '../../controllers/authController';
+import { confirmPassword, forgetPassword, login, logout, register, verifyOtp,verifyOtpForPassword, resetPassword } from '../../controllers/authController';
 
 const router = express.Router();
 
@@ -9,4 +9,7 @@ router.post('/confirm' , confirmPassword);
 router.post('/login' , login);
 router.post('/logout' , logout);
 
+router.post('/forget-password', forgetPassword)
+router.post('/verify', verifyOtpForPassword)
+router.post('/reset-password', resetPassword)
 export default router
